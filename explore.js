d44b4e09d0e3e4e4b3c8dcfe8ba84e1aa5f2422e
rakibulfarhad <mraisfa@gmail.com>
2021-09-05 00:52:12 +0600

    first commit

---
+++ explore.js
@@ -0,0 +1,10 @@
+const name = 15;
+const student = { 
+    name:'Jolil', friend:'borsha', job:'business',
+    makeMovie: function(name){
+        console.log(name)
+    }
+    
+};
+student.makeMovie('Din Raat')
+// document.getElementById('name');
